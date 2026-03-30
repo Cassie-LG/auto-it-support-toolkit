0cb2d64050dd9183c1d8499d5d15c9b867a71ade
clobogon <clobogon@purdue.edu>
2026-03-30 19:41:47 -0400

    Cross-platform monitoring

--- monitor.py
+++ monitor.py
@@ -1,10 +1,15 @@
 import psutil
+import os
+
+# Cross-platform disk path
+DISK_Path = "C:/" if os.name == "nt" else "/"
 
 # Collect system stats
 def get_system_stats():
     return {
         "cpu": psutil.cpu_percent(interval=1),     #CPU usage %
         "memory": psutil.virtual_memory().percent, #RAM usage %
-        "disk": psutil.disk_usage('/').percent,    #Disk usage %
-        "processes": [(p.pid, p.info['name'], p.info['cpu_percent']) for p in psutil.process_iter(['name', 'cpu_percent'])]
+        "disk": psutil.disk_usage(DISK_Path).percent,    #Disk usage %
+        "processes": [(p.pid, p.info['name'], p.info['cpu_percent'])
+                       for p in psutil.process_iter(['name', 'cpu_percent'])]
     }
