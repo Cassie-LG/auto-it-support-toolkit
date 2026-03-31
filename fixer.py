580c6dfcfdadeec721f96d3e2cb4389a15690761
clobogon <clobogon@purdue.edu>
2026-03-30 20:16:06 -0400

    Update Windows Process Fix

--- fixer.py
+++ fixer.py
@@ -5,25 +5,27 @@ SYSTEM = platform.system()
 
 #Attemp automatic fixes
 def fix_issue(issue):
+    #Disk Fix
     if issue["type"] == "DISK":
         try:
             if SYSTEM == "Windows":
                 os.system("del /q/f/s %TEMP%\\*")
                 return "Cleared Windows temp files"
             else:
-                os.system("rm -rf /tmp/*")     #Clear temp files
+                os.system("rm -rf /tmp/*")
                 return "Cleared /tmp directory"
         except Exception:
             return "Disk cleanup failed"
-        
+
+    #CPU Fix    
     elif issue["type"] == "CPU":
         return "Manual review recommended"
     
+    #Process Fix
     elif issue["type"] == "PROCESS":
-        try:
+        try: 
             if SYSTEM == "Windows":
-                os.system("echo Attempted restart (simulated)")
-                return "Simulated service restart (Windows)"
+                return "Critical system process missing - manual intervention required"
             else:
                 os.system("systemctl restart ssh")
                 return "Attempted to restart SSH"
