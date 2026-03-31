277d03a5939dd83d54eff17af93e78158dcd76b8
clobogon <clobogon@purdue.edu>
2026-03-30 20:25:46 -0400

    Syntax fix

--- monitor.py
+++ monitor.py
@@ -2,14 +2,14 @@ import psutil
 import os
 
 # Cross-platform disk path
-DISK_Path = "C:/" if os.name == "nt" else "/"
+DISK_PATH = "C:/" if os.name == "nt" else "/"
 
 # Collect system stats
 def get_system_stats():
     return {
-        "cpu": psutil.cpu_percent(interval=1),     #CPU usage %
-        "memory": psutil.virtual_memory().percent, #RAM usage %
-        "disk": psutil.disk_usage(DISK_Path).percent,    #Disk usage %
+        "cpu": psutil.cpu_percent(interval=1),           #CPU usage %
+        "memory": psutil.virtual_memory().percent,       #RAM usage %
+        "disk": psutil.disk_usage(DISK_PATH).percent,    #Disk usage %
         "processes": [(p.pid, p.info['name'], p.info['cpu_percent'])
                        for p in psutil.process_iter(['name', 'cpu_percent'])]
     }
